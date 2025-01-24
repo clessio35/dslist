f029b661484f9a5f1353e0a41f9c04eb5adc97b8
clessio35 <clessio.silva1989@gmail.com>
2025-01-24 18:58:14 -0300

    Move games on List

--- src/main/java/com/devsuperior/dslist/services/GameService.java
+++ src/main/java/com/devsuperior/dslist/services/GameService.java
@@ -35,7 +35,7 @@ public class GameService {
 	public List<GameMinDTO> findByList(Long listId){
 		List<GameMinProjection> result = gameRepository.searchByList(listId);
 		return result.stream().map(x -> new GameMinDTO(x)).toList();
-		
 	}
+	
 	
 }
